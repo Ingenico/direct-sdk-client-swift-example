302d471663676cadb68d73e134fef9e3f2a2b5a4
Ingencio dev team <60233882+ingenico-dev-team@users.noreply.github.com>
2021-02-24 16:20:57 +0100

    Release 1.0.0.

---
+++ Pods/Target Support Files/IngenicoDirectKit/IngenicoDirectKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IngenicoDirectKit : NSObject
+@end
+@implementation PodsDummy_IngenicoDirectKit
+@end
---
+++ Pods/Target Support Files/Pods-IngenicoDirectExampleTests/Pods-IngenicoDirectExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IngenicoDirectExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_IngenicoDirectExampleTests
+@end
